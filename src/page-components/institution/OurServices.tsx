208beba7c83081908e552a11a79e3752a9f7cd3a
PriyanshuKharel <kharelpriyanshu01@gmail.com>
2024-09-17 13:01:22 +0545

    Feat:instituion design update

--- src/page-components/institution/OurServices.tsx
+++ src/page-components/institution/OurServices.tsx
@@ -2,45 +2,7 @@
 import { RootContainer } from "@/components/common";
 import { Box, Typography, Grid, useTheme, useMediaQuery } from "@mui/material";
 import React from "react";
-
-const serviceItems = [
-  {
-    imgSrc: "/images/institution/one.svg",
-    title: "Recruitment of exceptional foreign students",
-    description:
-      "Partnering with WiseAdmit increases your university's visibility among top international students. It simplifies the application process, making it easier for students to apply.",
-  },
-  {
-    imgSrc: "/images/institution/two.svg",
-    title: "Technological partnership",
-    description:
-      "WiseScore® simplifies program matching, helping students find the best academic fit. It enhances your admissions system with seamless integration, real-time insights, and a user-friendly interface.",
-  },
-  {
-    imgSrc: "/images/institution/third.svg",
-    title: "Raising brand awareness and visibility",
-    description:
-      "We boost your brand’s visibility with social media management, strategic campaigns, and online advertising to attract students. We also organize virtual open houses and webinars to showcase your campus and programs effectively.",
-  },
-  {
-    imgSrc: "/images/institution/four.svg",
-    title: "Quality compliance",
-    description:
-      "We increase applicant reach and quality by partnering with top recruitment agents in key markets, ensuring they adhere to strict quality and ethical standards. We also provide them with training and resources to maximize their effectiveness.",
-  },
-  {
-    imgSrc: "/images/institution/five.svg",
-    title: "Strategic partnership with local high schools",
-    description:
-      "We build a pipeline of qualified students by partnering with top local high schools, organizing informational sessions, and hosting campus tours to engage potential applicants.",
-  },
-  {
-    imgSrc: "/images/institution/sixth.svg",
-    title: "Online lessons to boost awareness",
-    description:
-      "We boost awareness of your university by offering online language courses that highlight cultural and academic benefits while engaging a broader international audience.",
-  },
-];
+import { serviceItems } from "@/page-components/institution/utils/provider";
 
 const OurServices = () => {
   const theme = useTheme();
--- src/page-components/institution/utils/provider.tsx
+++ src/page-components/institution/utils/provider.tsx
@@ -19,6 +19,45 @@ export const menuListItem = [
   },
 ];
 
+export const serviceItems = [
+  {
+    imgSrc: "/images/institution/one.svg",
+    title: "Recruitment of exceptional foreign students",
+    description:
+      "Partnering with WiseAdmit increases your university's visibility among top international students. It simplifies the application process, making it easier for students to apply.",
+  },
+  {
+    imgSrc: "/images/institution/two.svg",
+    title: "Technological partnership",
+    description:
+      "WiseScore® simplifies program matching, helping students find the best academic fit. It enhances your admissions system with seamless integration, real-time insights, and a user-friendly interface.",
+  },
+  {
+    imgSrc: "/images/institution/third.svg",
+    title: "Raising brand awareness and visibility",
+    description:
+      "We boost your brand’s visibility with social media management, strategic campaigns, and online advertising to attract students. We also organize virtual open houses and webinars to showcase your campus and programs effectively.",
+  },
+  {
+    imgSrc: "/images/institution/four.svg",
+    title: "AI interviewer",
+    description:
+      "Our AI-powered interview platform conducts automated candidate interviews, evaluating their responses in real-time. It provides objective scoring based on key performance metrics, ensuring a fair and efficient hiring process.",
+  },
+  {
+    imgSrc: "/images/institution/five.svg",
+    title: "Strategic partnership with local high schools",
+    description:
+      "We build a pipeline of qualified students by partnering with top local high schools, organizing informational sessions, and hosting campus tours to engage potential applicants.",
+  },
+  {
+    imgSrc: "/images/institution/sixth.svg",
+    title: "Online lessons to boost awareness",
+    description:
+      "We boost awareness of your university by offering online language courses that highlight cultural and academic benefits while engaging a broader international audience.",
+  },
+];
+
 export const galleryImages = [
   "/images/institution/1.jpg",
   "/images/institution/2.jpg",
