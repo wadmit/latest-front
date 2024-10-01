d0cf177f657e278046de6df7d81ce7ee7662f553
SUBHAM MISHRA <subhammishra133@gmail.com>
2024-10-01 10:24:50 +0545

    Update: Remove Console

--- src/page-components/scanned/index.tsx
+++ src/page-components/scanned/index.tsx
@@ -1,11 +1,13 @@
-'use client'
-import React, { useEffect } from 'react'
-import API from "@/constants/app"
-import { Typography } from '@mui/material';
-import { Loader, RootContainer } from '@/page-components/scanned/styled-components';
-import { useRouter, useSearchParams } from 'next/navigation';
-import axios from 'axios';
-
+"use client";
+import React, { useEffect } from "react";
+import API from "@/constants/app";
+import { Typography } from "@mui/material";
+import {
+  Loader,
+  RootContainer,
+} from "@/page-components/scanned/styled-components";
+import { useRouter, useSearchParams } from "next/navigation";
+import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_BASE_URL}/${API.API_VERSION}`;
 
@@ -13,15 +15,13 @@ const ScannedHome = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-    const uuid = searchParams.get('uuid');
-    const source = searchParams.get('source');
-    console.log(uuid, source);
-
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(`${URL}/web/qr-data/${uuid}`);
-            if (response) {
-                // console.log(response);
+  const uuid = searchParams.get("uuid");
+  const source = searchParams.get("source");
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${URL}/web/qr-data/${uuid}`);
+      if (response) {
+        // console.log(response);
 
         // Data retrieval successful
         // Process the data here if needed
